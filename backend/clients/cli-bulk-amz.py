8962b19b0350a6f677c5d93a5c8163fe810d5fa8
Yixing Lin <184059914@qq.com>
2024-08-30 20:02:36 +0200

    Update requirements.txt

--- backend/clients/cli-bulk-amz.py
+++ backend/clients/cli-bulk-amz.py
@@ -3,6 +3,7 @@ import sys
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
 import requests
+from requests.auth import HTTPBasicAuth
 from utils import utilpdf
 from utils import stringutils
 import easygui
@@ -20,27 +21,36 @@ import easygui
 """
 
 URL_MAP = {
-    'dev': "http://192.168.8.140:5018",
+    'dev': "http://192.168.8.70:5018",
     'stage': "http://192.168.8.10:5018",
     'prod': "http://192.168.8.95:5018"
 }
 
 BASE_URL = URL_MAP['dev']
+username = ""
+password = ""
 
 root = ".temp"
 api_key_index = 0     # Amazon API key index
 
+def get_method(*args, **kwargs):
+    return requests.get(auth=HTTPBasicAuth(username, password), *args, **kwargs)
+
+def post_method(*args, **kwargs):
+    return requests.post(auth=HTTPBasicAuth(username, password), *args, **kwargs)
+
 def update_database():
     params = {"api_key_index": api_key_index, "up_to_date": True}
-    requests.get(f"{BASE_URL}/api/v1/amazon/orders/unshipped", params=params)
+    get_method(f"{BASE_URL}/api/v1/amazon/orders/unshipped",
+                 params=params)
 
 def display_unshipped_orders():
     # 获取未发货订单号。
     params = {"api_key_index": api_key_index, "up_to_date": True}
-    resp = requests.get(f"{BASE_URL}/api/v1/amazon/orders/unshipped", params=params)
+    resp = get_method(f"{BASE_URL}/api/v1/amazon/orders/unshipped", params=params)
     orderIds = resp.json()['data']['orderNumbers']
     # 生成装箱单下载链接
-    resp = requests.get(f"{BASE_URL}/api/v1/amazon/orders/sc-urls")
+    resp = get_method(f"{BASE_URL}/api/v1/amazon/orders/sc-urls")
     data = resp.json()['data']
     pack_slip_url = f"{data['packing-slip']}/?orderIds={';'.join(orderIds)}"
     print(f"装箱单下载链接: {pack_slip_url}\n")
@@ -53,13 +63,13 @@ def sim_upload_amazon_pack_slips():
 
     # Download a packing slip from the website
     url = "https://www.hamster25.buzz/amazon/amazon-delivery-de-07.html"
-    response = requests.get(url)
+    response = get_method(url)
     text = response.text.encode('utf-8')
     msg = "装箱单下载成功，准备上传到服务器"
     print(msg)
     easygui.msgbox(msg, title="装箱单下载成功, 准备上传到服务器")
     endpoint = BASE_URL + "/api/v1/amazon/orders/packslip/cache"
-    resp = requests.post(endpoint, data=text)
+    resp = post_method(endpoint, data=text)
     key = resp.json()['data']['key']
     msg = f"装箱单上传成功，Key = {key}"
     print(msg)
@@ -81,7 +91,8 @@ def upload_amazon_pack_slips_via_file():
         html_content = f.read()
     endpoint = BASE_URL + "/api/v1/amazon/orders/packslip/cache"
     headers = {'Content-Type': 'text/html'}
-    resp = requests.post(endpoint, data=html_content.encode('utf-8'), headers=headers)
+    resp = post_method(endpoint, data=html_content.encode('utf-8'), headers=headers)
+    print(resp.content)
     key = resp.json()['data']['key']
     print(f"装箱单上传成功，Key = {key}\n")
     return key
@@ -91,7 +102,7 @@ def test_amazon_packslip_parser(key):
     input(f"Step 2: 从服务器下载暂存的装箱单，按任意键继续。。。\nKey={key}")
     endpoint = BASE_URL + "/api/v1/amazon/orders/packslip/uncache"
     params = {"key": key}
-    resp = requests.get(endpoint, params=params)
+    resp = get_method(endpoint, params=params)
     text = resp.text
     print("装箱单解析成功，数据如下：\n")
     print(text[1000:1200], "...\n")
@@ -106,7 +117,7 @@ def test_amazon_packslip_parser(key):
     }
 
     # Send the request to the server to parse the packing slip
-    resp = requests.post(endpoint, json=body)
+    resp = post_method(endpoint, json=body)
     print(resp.url)
     data = resp.json()['data']
     orderIds = [od.get('orderId') for od in data.get('orders')]
@@ -121,7 +132,7 @@ def test_amazon_packslip_parser(key):
 def test_create_parcel_labels(orderIds):
     input(f"Step 3: 批量生成运单，这个过程可能持续几分钟，请耐心等待。。。按任意键继续。。。")
     endpoint = BASE_URL + "/api/v1/pickpack/amazon/bulk-ship/gls"
-    resp = requests.post(endpoint, json=orderIds)
+    resp = post_method(endpoint, json=orderIds)
     print(resp.url)
     if resp.status_code != 200:
         print(resp.text)
@@ -144,7 +155,7 @@ def test_get_manifest(batchId):
     os.makedirs(folder, exist_ok=True)
     print(f"文件夹创建成功: {folder}\n")
 
-    resp = requests.get(f"{BASE_URL}/api/v1/pickpack/common/batch-ship-event", params={'batchId': batchId})
+    resp = get_method(f"{BASE_URL}/api/v1/pickpack/common/batch-ship-event", params={'batchId': batchId})
     data = resp.json()['data']
     print(resp.url)
     orderIds = data['orderIds']
@@ -164,7 +175,7 @@ def test_get_manifest(batchId):
     easygui.msgbox(msg, title="保存成功")
     os.startfile(folder)
 
-    resp = requests.get(f"{BASE_URL}/api/v1/amazon/orders/sc-urls")
+    resp = get_method(f"{BASE_URL}/api/v1/amazon/orders/sc-urls")
     data = resp.json()['data']
     bulk_confirm_shipment_url = f"{data['bulk-confirm-shipment']}/{';'.join(orderIds)}"
 
@@ -184,19 +195,19 @@ def test_get_manifest(batchId):
     input("按任意键开始下载拣货单和订单列表。。。\n")
     # Save pick slip as excel
     refs = ";".join(orderIds)
-    resp = requests.get(f"{BASE_URL}/api/v1/pickpack/amazon/batch-pick", params={'refs': refs})
+    resp = get_method(f"{BASE_URL}/api/v1/pickpack/amazon/batch-pick", params={'refs': refs})
     print(resp.url)
     with open(f'{folder}/pick-slip.xlsx', 'wb') as f:
         f.write(resp.content)
 
     # Save pack slip as excel
-    resp = requests.get(f"{BASE_URL}/api/v1/pickpack/amazon/batch-pack", params={'refs': refs})
+    resp = get_method(f"{BASE_URL}/api/v1/pickpack/amazon/batch-pack", params={'refs': refs})
     print(resp.url)
     with open(f'{folder}/pack-slip.xlsx', 'wb') as f:
         f.write(resp.content)
 
     # Save parcels as pdf
-    resp = requests.get(f"{BASE_URL}/api/v1/carriers/gls/shipments/bulk-labels", params={'refs': refs})
+    resp = get_method(f"{BASE_URL}/api/v1/carriers/gls/shipments/bulk-labels", params={'refs': refs})
     print(resp.url)
     data: bytes = resp.content
     with open(f'{folder}/parcels-V2-mongo.pdf', 'wb') as f:
